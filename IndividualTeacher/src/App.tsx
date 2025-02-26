358484b1ff1fbfbdb48403d85297e489dadc0e58
cruetto <131352364+cruetto@users.noreply.github.com>
2025-02-26 20:45:29 +0000

    Added basic design: Quiz section, Chat section and Saved Quizzes section

--- IndividualTeacher/src/App.tsx
+++ IndividualTeacher/src/App.tsx
@@ -1,35 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import ChatApp from "./components/Chat";
+import Quiz from "./components/Quiz";
+import QuizManager from "./components/QuizManager";
 
 function App() {
-  const [count, setCount] = useState(0)
+  let answers = [
+    "Answer asdfafasdfasdfasdfasdfas asdfafasdfasdfasdfasdfasd1",
+    "Answer 2 asdfafasdfasdfasdfasdfas asdfafasdfasdfasdfasdfas",
+    "Answer  asdfafasdfasdfasdfasdfas3",
+    "Answer 4 asdfafasdfasdfasdfasdfas",
+  ];
+  const handleSelectItemAnswer = (answer: string) => {
+    console.log(answer);
+  };
+
+  // let quizzes = ["Computer Science", "Biology"];
+  // const handleSelectItemQuiz = (answer: string) => {
+  //   console.log(answer);
+  // };
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <QuizManager></QuizManager>
+
+      <Quiz
+        answers={answers}
+        heading="Questionasdfasdfasdfa?"
+        onSelectItem={handleSelectItemAnswer}
+      ></Quiz>
+
+      <ChatApp></ChatApp>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
